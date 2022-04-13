81ae91cbd4fcccb68b5defef9462e14456774436
Emir Savranogullari <emirsavranogullari@gmail.com>
2022-04-13 15:53:14 +0300

    Trigger Firebase Hosting workflow

--- packages/tasty-webapp/src/App.tsx
+++ packages/tasty-webapp/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Tasty DAO</p>
+        <p>Tasty DAO Home</p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
